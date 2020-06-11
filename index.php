2bfd5e3cb0993668ba37f33ebeb8338edcf44e8f
Alex-E-Walsh <aewalsh16@gmail.com>
2020-06-11 13:08:04 -0700

    quick bug edits

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8">
     <meta name="description" >
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>The Personal Spotify Playlist Generator</title>
+    <title>Playlist Generator</title>
     <link rel="stylesheet" href="static/style.css">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
