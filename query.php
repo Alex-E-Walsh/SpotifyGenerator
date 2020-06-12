23df164127bf9472d97b804bde9c42f57bc9d717
Alex-E-Walsh <aewalsh16@gmail.com>
2020-06-11 18:02:24 -0700

    heroku

--- query.php
+++ query.php
@@ -5,22 +5,17 @@
 // $password = "eec042e14e6ca3afe7e68edf5de170ee293b790230a29dd719feb2e2d3fa6f9c";
 // $dbname = "d9pugpe7g2t1e6";
 // $port = "5432";
-$db = parse_url(getenv("DATABASE_URL"));
-$db["path"] = ltrim($db["path"], "/");
 
-if (!$db){
-    echo "UNABLE TO CONNECT TO DATABASE.\n";
-    exit;
-  }
-// $db_connect = pg_connect("postgres://ydshrbbwrnvyrd:c96b22a2f1b5ddfc521871921486eafeb0170eef63a5cce698c49b79d02697cb@ec2-35-174-127-63.compute-1.amazonaws.com:5432/dql5qdqd4414m");
+// $connstring = "host=ec2-35-174-127-63.compute-1.amazonaws.com dbname=dql5qdqd4414m user=ydshrbbwrnvyrd password=c96b22a2f1b5ddfc521871921486eafeb0170eef63a5cce698c49b79d02697cb";
 
+$db_connect = pg_connect(getenv("DATABASE_URL"));
 
   if(!empty($_POST['search'])){
     $db_connect = pg_connect(getenv($dsn));
     if (!$db_connect){
         echo "UNABLE TO CONNECT TO DATABASE.\n";
         exit;
-      }
+      };
     $q = '%'.pg_escape_string($db_connect, $_POST['q']).'%';
     $sql = pg_query($db_connect,"SELECT title, artists, id FROM cleaned_songs WHERE title ILIKE '$q' or artists ILIKE '$q' ORDER BY popularity DESC LIMIT 15");
 
